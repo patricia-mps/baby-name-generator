5d9b706f1fef3bc61577f2ca4c7d33d6dfa56a80
Patrícia Silva <patricia.mps85@gmail.com>
2023-02-27 17:05:42 +0000

    add page notFound

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,12 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Homepage from '../pages/homepage';
+import PageNotFound from '../pages/pageNotFound';
 
 const Router = () => (
   <HashRouter>
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   </HashRouter>
 );
